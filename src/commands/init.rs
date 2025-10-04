a9ff13c66ee2a8f60c21e0dba51ac46a35b07789
Nima Naseri <77214940+naseridev@users.noreply.github.com>
2025-10-04 06:58:37 -0700

    Refactored and imporved minor commands #2

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -23,7 +23,6 @@ impl Init {
         }
 
         let confirm_password = UserPrompt::password("Confirm password: ")?;
-
         Password::match_check(master_password.as_str(), confirm_password.as_str())?;
 
         let entry = Crypto::new(&master_password).create_entry(get_test_data(), None)?;
